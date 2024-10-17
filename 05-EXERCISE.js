de8fa6e670a21cb077752c8b83e8ab603af30825
Marshall-Bits <kz_@hotmail.es>
2024-10-17 13:02:25 +0200

    add exercise

--- 05-EXERCISE.js
+++ 05-EXERCISE.js
@@ -1,8 +1,22 @@
-const team = [
-    "Dani", 
-    "Diego", 
-    "Kara",
-    "Nigel", 
-    "Piet", 
-];
+const students = ["Dani", "Diego", "Kara", "Nigel", "Piet"];
 
+students.forEach((student) => {
+  console.log(student.toUpperCase());
+});
+
+console.log(students);
+
+const string = "hellooooo";
+
+const stringArray = string.split("");
+
+console.log(stringArray);
+
+const reversedArray = stringArray.reverse();
+
+console.log(reversedArray);
+
+console.log(reversedArray.toString().replaceAll(",", ""));
+
+const reversedString = reversedArray.join("");
+console.log(reversedString);
